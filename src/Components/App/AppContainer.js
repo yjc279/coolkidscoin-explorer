802ceafff14f63b0181ff6763d6357292515c871
YJC mac <yoon_je_chung@solomonsc.com>
2018-08-03 15:34:17 +0900

    txs flatten

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -1,18 +1,13 @@
-import React, {
-  Component
-} from "react";
-import {
-  injectGlobal
-} from "styled-components";
+import React, { Component } from "react";
+import { injectGlobal } from "styled-components";
 import reset from "styled-reset";
 import axios from "axios";
+import flatten from "lodash.flatten";
 import AppPresenter from "./AppPresenter";
 import typography from "../../typography";
-import {
-  API_URL
-} from "../../constants";
+import { API_URL } from "../../constants";
 
-const baseStyles = () => injectGlobal `
+const baseStyles = () => injectGlobal`
     ${reset};
     ${typography};
     a{
@@ -29,13 +24,16 @@ class AppContainer extends Component {
   };
   render() {
     baseStyles();
-    return <AppPresenter { ...this.state} />;
+    return <AppPresenter {...this.state} />;
   }
   _getData = async () => {
     const request = await axios.get(`${API_URL}/blocks`);
     const blocks = request.data;
+    const reversedBlocks = blocks.reverse();
+    const txs = flatten(reversedBlocks.map(block => block.data));
     this.setState({
-      blocks,
+      blocks: reversedBlocks,
+      transactions: txs,
       isLoading: false
     });
   };
