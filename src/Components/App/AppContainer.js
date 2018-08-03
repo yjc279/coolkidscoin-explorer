90f185ccf016226e8d607a6248eaca08ddccc10b
YJC mac <yoon_je_chung@solomonsc.com>
2018-08-03 15:13:32 +0900

    blocks axios

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -1,20 +1,44 @@
-import React, { Component } from "react";
-import { injectGlobal } from "styled-components";
-import AppPresenter from "./AppPresenter";
+import React, {
+  Component
+} from "react";
+import {
+  injectGlobal
+} from "styled-components";
 import reset from "styled-reset";
+import axios from "axios";
+import AppPresenter from "./AppPresenter";
 import typography from "../../typography";
+import {
+  API_URL
+} from "../../constants";
 
-
-const baseStyles = () => injectGlobal`
+const baseStyles = () => injectGlobal `
     ${reset};
     ${typography};
+    a{
+        text-decoration:none!important;
+    }
 `;
 
 class AppContainer extends Component {
-    render () {
-        baseStyles();
-        return <AppPresenter />;
-    }
+  state = {
+    isLoading: true
+  };
+  componentDidMount = () => {
+    this._getData();
+  };
+  render() {
+    baseStyles();
+    return <AppPresenter { ...this.state} />;
+  }
+  _getData = async () => {
+    const request = await axios.get(`${API_URL}/blocks`);
+    const blocks = request.data;
+    this.setState({
+      blocks,
+      isLoading: false
+    });
+  };
 }
 
 export default AppContainer;
